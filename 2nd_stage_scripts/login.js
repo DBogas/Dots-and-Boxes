6fefaccb83e3955cbe1cc92bbfe98c0be578a250
Yisgramiir <dbogas738@gmail.com>
2016-11-25 10:13:59 +0000

    now able to login to sv

--- 2nd_stage_scripts/login.js
+++ 2nd_stage_scripts/login.js
@@ -1,35 +1,24 @@
-/*
-readystate:
-0 - not initialized
-1 - open
-2 - request sent
-3 - receiving answer
-4 - answer received
-*/
-
 function register_me(){
-var name = document.getElementById("id").value.toString();
-var pass = document.getElementById("pw").value.toString();
-var params = "name="+name+"&pass="+pass;
+
+var nome = document.getElementById("id").value.toString();
+var pw = document.getElementById("pw").value.toString();
+var params = JSON.stringify({name: nome , pass: pw});
 var register_req = new XMLHttpRequest();
 
-register_req.open("post","http://twserver.alunos.dcc.fc.up.pt:8042/register",true);
+register_req.open("post","http://twserver.alunos.dcc.fc.up.pt:8000/register",true);
 register_req.setRequestHeader("Content-type", "application/json");
 
 register_req.onreadystatechange = function(){
-    // enquanto n receber resposta
-    if(register_req.readyState != 4){
+    if(register_req.readyState !== 4){
         return;
     }
-    // tratamento de erros
-    if(register_req.status != 200){
+    if(register_req.status !== 200){
         window.alert("Error - Bad Request, error: "+register_req.status+" readystate: "+register_req.readyState);
         return;
     }
     var sv_response = register_req.responseText;
-    // fazer malabarismos
-    switchDiv('log_in_div','main_menu')
+    if(sv_response === "{}") switchDiv('log_in_div','main_menu');
+    else window.alert("Error: "+sv_response);
 }
-
-register_req.send(params);
+    register_req.send(params);
 }
