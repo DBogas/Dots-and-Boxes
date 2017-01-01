52d389897485e28ba1b3c4c082c39856976556a7
Yisgramiir <dbogas738@gmail.com>
2017-01-01 22:40:49 +0000

    added comments to the code for future reference

--- ServerScripts/MainServer.js
+++ ServerScripts/MainServer.js
@@ -1,4 +1,4 @@
-//Lets require/import the HTTP module
+//Lets import here all the module we use
 var http = require('http');
 var url = require('url');
 var formidable = require('formidable');
@@ -16,14 +16,12 @@ app.use(parser.urlencoded( {
     extended: true
 }));
 
+
 var db = mysql.createConnection({
     host: 'localhost',
     // credenciais 1
     user: 'up201200296',
-    password: 'kappa123',
-    
-    // credenciais 2
-    //user: 'up201202482'
+    password: 'kappa123'
 });
 
 db.connect(function(error)  {
@@ -37,6 +35,7 @@ db.connect(function(error)  {
 //chamar modulo crypto para encriptaçao de passwords
 var crypto = require('crypto');
 
+//create an hash to protect user's passwords
 function createHash(pass) {
     return crypto.createHash('md5').update(pass).digest('hex');
 }
@@ -45,6 +44,7 @@ var RSG = new chance();
 
 //register
 /*
+para fazer este metodo, ter em conta este comentario multi linha
 
 Para as passwords:
 Comparaçao:
@@ -56,36 +56,44 @@ o salt sao 4 chars aleatorios, usando modulo chance
 
 Na insercao de novos users na database:
 de acordo com a tabela Users : name, pass, salt
+
 */
+
 app.post('/register', function(request, response) {
-    // validator stuff
+    // get the data that comes in te request
     var req_name = request.body.name;
     var req_pass = request.body.pass;
-    
+    // check for name validity (sql injection preventing)
     if(validator.isAlphanumeric(req_name)) {
+        // do a query for all users that have the name we saw before, assuming we saw one
+        // only has one user.
         var query_this = db.query('select * from Users where name = ?', [req_name], function(err, answer) {
+            // error handling
             if(err) console.log(err);
-            
+            // if there is a record of a player with that name, check the password
             if(answer.length > 0) {
                 console.log("User exists");
                 var player = answer[0];
-                
+                // password checks
                 if(createHash(req_pass + player.salt) == player.pass) {
                     console.log("Success");
                     response.json({});
                 }
+                //password doens't fit
                 else {
                     console.log("dats wrung");
                     response.json({"error": "Utilizador registado com password diferente"});
                 }
             }
+            // no user in the database? register a new one
             else {
                 console.log("New user");
-                
+                // create a salt and hash
                 var playerSalt = RSG.string({length : 4});
                 var playerHash = createHash(req_pass + playerSalt);
-                
+                //create the object to store in the db
                 var post = {name : req_name, pass : playerHash, salt : playerSalt};
+                //make a query to insert the new player
                 var query = db.query('insert into Users set ?', [post], function(err, answer) {
                     if(err) console.log(err);
                     console.log("User registred");
@@ -94,11 +102,15 @@ app.post('/register', function(request, response) {
             }
         });
     }
+    // happens when a given id isnt valid
     else {
         response.json({"erro": "Jogador invalido"});
     }
 });
-
+/*
+after catching the difficulty from the request, make a query
+this query orders all scores descending, selects top 10, returns in json
+*/
 app.post('/ranking', function(request, response) {
     var diff = request.body.level;
     var query = db.query('SELECT * FROM Rankings WHERE level = ? ORDER BY boxes DESC, time ASC LIMIT 10;', [diff], function(err, answer) {
