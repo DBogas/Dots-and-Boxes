1429ecfc8dcf0af77bd6dde1a36f52b675c7920a
Yisgramiir <dbogas738@gmail.com>
2017-01-01 22:21:26 +0000

    Deleted 1 file

--- ServerScripts/db.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE Users(name VARCHAR(32) NOT NULL, pass VARCHAR(32) NOT NULL, salt VARCHAR(4) NOT NULL, PRIMARY KEY (name) ) ENGINE=INNODB; 
-CREATE TABLE Rankings ( name VARCHAR(32) NOT NULL, level ENUM('beginner', 'intermediate', 'advanced', 'expert') NOT NULL, boxes INT NOT NULL, time INT NOT NULL, INDEX (level, boxes, time), FOREIGN KEY (name) REFERENCES Users (name) ON DELETE CASCADE ) ENGINE=INNODB; 
